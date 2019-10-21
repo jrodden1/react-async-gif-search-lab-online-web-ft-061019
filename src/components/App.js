944da00dd9b9f0ff763c4fb979f5339abc42e8f8
Jeremiah Rodden <jrodden1@gmail.com>
2019-10-21 15:26:44 -0600

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,17 @@
 import React from 'react'
-
+import GifListContainer from '../containers/GifListContainer.js';
 import NavBar from './NavBar'
 
 // the App component should render out the GifListContainer component 
+// http://api.giphy.com/v1/gifs/search?q=YOUR QUERY HERE&api_key=dc6zaTOxFJmzC&rating=g
+// forEach snag this url and post it - data.images.original.url
+
 
 const App = () => {
   return (
-    <div>
-        < NavBar color='black' title="Giphy Search" />
+    <div className="container">
+        <NavBar color='black' title="Giphy Search" />
+        <GifListContainer />
     </div>
   )
 }
